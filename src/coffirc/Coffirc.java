b9ca1b72bdd7e4648ed1f3b43df7f83da06e1624
Jim Read <jimmydean886@hotmail.com>
2017-03-25 19:17:29 -0400

    Converted coffirc.java to tabs
    Used 4 space indentation, the way of plebs. Uses tabs now.

--- src/coffirc/Coffirc.java
+++ src/coffirc/Coffirc.java
@@ -8,90 +8,95 @@ import java.util.Scanner;
  */
 public class Coffirc {
 
-    /**
-     * @param args
-     * @throws java.lang.Exception
-     */
-    public static void main(String[] args) throws Exception {
-        
-        String rawInput;
-        Connection connection = null;
-        
-        System.out.println("coffirc v0.1.0");
-        
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                /*
-                if (connection != null && connection.stage == 2){
-                    connection.readLines();
-                }*/
-                
-                //System.out.print("> ");
-                rawInput = scanner.nextLine();
-                //System.out.print("> ");
-                
-                if (rawInput.charAt(0) == '/') {
-                    String[] commArr = rawInput.substring(1).split(" ");
-                    String command = commArr[0];
-                    
-                    if ("connect".equals(command)){
-                        if (connection == null){
-                            if (commArr.length == 3){
-                                connection = new Connection(commArr[1], Integer.parseInt(commArr[2])); 
-                            }
-                            else {      
-                                System.err.println("Usage: /connect <server> <port>");
-                            } 
-                        }
-                        else {
-                            System.err.println("Already connected. Use /disconnect.");
-                        }
-                    }
-                    else {
-                        if (connection == null && command.equals("quit") == false) {
-                            System.err.println("Not connected.");
-                        }
-                        else {
-                            // I really hate if-else-if ladders but switch:case doesn't work here
-                            System.out.println(command);
-                            
-                            if("disconnect".equals(command)){
-                                connection.close();
-                                System.err.println("Disconnected.");
-                            }
-                            else if ("quit".equals(command)) {
-                                System.out.println("Program terminated.");
-                                break;
-                            }
-                            else if ("nick".equals(command)) {
-                                connection.changeNick(commArr[1]);
-                            }
-                            else if ("part".equals(command)) {
-                                connection.partChannel();
-                            }
-                            else if ("join".equals(command)) {
-                                if (commArr.length == 2){
-                                    connection.joinChannel(commArr[1], null);
-                                }
-                                else {
-                                    connection.joinChannel(commArr[1], commArr[2]);
-                                }
-                            }
-                            else if ("list".equals(command)) {
-                                connection.listChans();
-                            }
-                        }
-                    }
-                }
-                else {
-                    if (connection != null) {
-                        connection.sendPrivmsg(rawInput);
-                    }
-                    else {
-                        System.err.println("Not connected.");
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * @param args
+	 * @throws java.lang.Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		String rawInput;
+		Connection connection = null;
+		
+		System.out.println("coffirc v0.1.0");
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (true) {
+				/*
+				if (connection != null && connection.stage == 2){
+					connection.readLines();
+				}*/
+				
+				//System.out.print("> ");
+				rawInput = scanner.nextLine();
+				//System.out.print("> ");
+				
+				if (rawInput.charAt(0) == '/') {
+					String[] commArr = rawInput.substring(1).split(" ");
+					String command = commArr[0];
+					
+					if ("connect".equals(command)){
+						if (connection == null){
+							if (commArr.length == 3){
+								connection = new Connection(commArr[1], Integer.parseInt(commArr[2])); 
+							}
+							else {	  
+								System.err.println("Usage: /connect <server> <port>");
+							} 
+						}
+						else {
+							System.err.println("Already connected. Use /disconnect.");
+						}
+					}
+					else {
+						if (connection == null && command.equals("quit") == false) {
+							System.err.println("Not connected.");
+						}
+						else {
+							// I really hate if-else-if ladders but switch:case doesn't work here
+							System.out.println(command);
+							string commands[] = {"disconnect", "quit", "nick", "part", "join", "list"};
+							int cindex = 666;
+							for(int i = 0; i<commands.length; i++){
+								if(command == commands[i]) cindex = i;
+							}
+							switch(cindex){
+								case 0:
+									connection.close();
+									System.err.println("Disconnected.");
+									break;
+								case 1:
+									System.out.println("Program terminated.");
+									exit(0);
+									break;
+								case 2:
+									connection.changeNick(commArr[1]);
+									break;
+								case 3:
+									connection.partChannel();
+									break;
+								case 4:
+									if (commArr.length == 2){
+										connection.joinChannel(commArr[1], null);
+									}
+									else {
+										connection.joinChannel(commArr[1], commArr[2]);
+									}
+									break;
+								case 5:
+									connection.listChans();
+							}
+						}
+					}
+				}
+				else {
+					if (connection != null) {
+						connection.sendPrivmsg(rawInput);
+					}
+					else {
+						System.err.println("Not connected.");
+					}
+				}
+			}
+		}
+	}
 }
